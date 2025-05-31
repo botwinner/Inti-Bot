4f464b6a2888182dbcbb891c9b46d9629b117d32
St Manía <piterfreik@gmail.com>
2025-05-30 19:46:40 -0500

    Update config.js

--- config.js
+++ config.js
@@ -12,12 +12,12 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 global.owner = [
-["525665918012", 'st', true], //la barbie
-["525665918012", 'st', true], //es gay
-["525665918012"]]
+["5216942554318", 'st', true], //la barbie
+["5216942554318", 'st', true], //es gay
+["5216942554318"]]
 
-global.mods = ["525665918012"]
-global.prems = ["525665918012"]
+global.mods = ["5216942554318"]
+global.prems = ["5216942554318"]
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
@@ -112,9 +112,9 @@ global.moment = moment
 // [ES] > Agregate a ti, colaboradores o ayudates, aparecerá en el comando de lista de contactos.
 // [EN] > Adding yourself, collaborators or helpers will appear in the contact list command.
 global.official = [ // Agregate si eres Owner
-["525665918012", 'st', 1], 
-["525665918012", 'st', 1],  
-["525665918012", 'st', 1]]
+["5216942554318", 'st', 1], 
+["5216942554318", 'st', 1],  
+["5216942554318", 'st', 1]]
 
 global.mail = '' // Add email
 global.desc = '' // Add short description (20 caractres max)
@@ -122,8 +122,8 @@ global.desc2 = '' // Add long description (90 caractres max) (Este parámetro se
 global.country = '' // Add country, example: 🇪🇨
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
-global.packname = "𝐁𝐚𝐫𝐛𝐢𝐞𝗕𝗼𝘁"
-global.author = "𝐁𝐚𝐫𝐛𝐢𝐞𝗕𝗼𝘁"
+global.packname = "𝐈𝐧𝐭𝐢-𝐁𝐨𝐭"
+global.author = "𝐈𝐧𝐭𝐢-𝐁𝐨𝐭"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > INFORMACIÓN DE VERSIONES DEL BOT, POR FAVOR 
@@ -131,7 +131,7 @@ global.author = "𝐁𝐚𝐫𝐛𝐢𝐞𝗕𝗼𝘁"
 // [EN] > BOT VERSION INFORMATION, PLEASE KEEP THIS UNCHANGED, WE STRIVE DAILY TO PROVIDE YOU WITH A BOT FOR THE COMMUNITY, BE GRATEFUL
 global.vs = "1.7.0"
 global.vsJB = "2.5 (Beta)"
-global.gt = "𝐁𝐚𝐫𝐛𝐢𝐞𝗕𝗼𝘁"
+global.gt = "𝐈𝐧𝐭𝐢-𝐁𝐨𝐭"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
@@ -157,9 +157,9 @@ global.eg = '╰⊱💚⊱ *𝙀́𝙓𝙄𝙏𝙊 | 𝙎𝙐𝘾𝘾𝙀𝙎�
 global.exito = eg
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
-global.wm = "𝐁𝐚𝐫𝐛𝐢𝐞𝗕𝗼𝘁"
-global.igfg = "𝐁𝐚𝐫𝐛𝐢𝐞𝗕𝗼𝘁"
-global.nomorown = "525665918012"
+global.wm = "𝐈𝐧𝐭𝐢-𝐁𝐨𝐭"
+global.igfg = "𝐈𝐧𝐭𝐢-𝐁𝐨𝐭"
+global.nomorown = "5216942554318"
 global.pdoc = ["application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.ms-excel", "application/msword", "application/pdf", "text/rtf"]
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
